4c4236d93fd1b9593d6058f0cf66c713d9b60395
infan11 <infanjiounrahman20606@gmail.com>
2024-07-05 10:42:24 +0600

    TailwindConfig.js add in github

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Navbar/>
+            <Outlet/>
         </div>
     );
 };
